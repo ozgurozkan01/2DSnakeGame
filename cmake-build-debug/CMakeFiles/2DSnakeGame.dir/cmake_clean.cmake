a6be60dbafb6d9009cda548d103240f824ae6b15
ozgurozkan01 <90643276+ozgurozkan01@users.noreply.github.com>
2021-10-16 20:46:41 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/2DSnakeGame.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2DSnakeGame.exe"
+  "2DSnakeGame.exe.manifest"
+  "2DSnakeGame.pdb"
+  "CMakeFiles/2DSnakeGame.dir/main.cpp.obj"
+  "lib2DSnakeGame.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2DSnakeGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
