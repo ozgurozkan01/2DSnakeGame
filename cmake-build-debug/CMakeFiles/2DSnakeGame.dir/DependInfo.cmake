ab51acb9a4b1c4ac6b7fa762fdf9f5b15fb43946
ozgurozkan01 <90643276+ozgurozkan01@users.noreply.github.com>
2022-01-28 10:00:51 +0300

    save finished game

--- cmake-build-debug/CMakeFiles/2DSnakeGame.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/2DSnakeGame.dir/DependInfo.cmake
@@ -4,31 +4,16 @@ set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
 
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
-  )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/ozgur/Documents/C++Projects/2DSnakeGame/Bait.cpp" "C:/Users/ozgur/Documents/C++Projects/2DSnakeGame/cmake-build-debug/CMakeFiles/2DSnakeGame.dir/Bait.cpp.obj"
-  "C:/Users/ozgur/Documents/C++Projects/2DSnakeGame/Game.cpp" "C:/Users/ozgur/Documents/C++Projects/2DSnakeGame/cmake-build-debug/CMakeFiles/2DSnakeGame.dir/Game.cpp.obj"
-  "C:/Users/ozgur/Documents/C++Projects/2DSnakeGame/Game_Block.cpp" "C:/Users/ozgur/Documents/C++Projects/2DSnakeGame/cmake-build-debug/CMakeFiles/2DSnakeGame.dir/Game_Block.cpp.obj"
-  "C:/Users/ozgur/Documents/C++Projects/2DSnakeGame/Snake.cpp" "C:/Users/ozgur/Documents/C++Projects/2DSnakeGame/cmake-build-debug/CMakeFiles/2DSnakeGame.dir/Snake.cpp.obj"
-  "C:/Users/ozgur/Documents/C++Projects/2DSnakeGame/Snake_Part.cpp" "C:/Users/ozgur/Documents/C++Projects/2DSnakeGame/cmake-build-debug/CMakeFiles/2DSnakeGame.dir/Snake_Part.cpp.obj"
-  "C:/Users/ozgur/Documents/C++Projects/2DSnakeGame/main.cpp" "C:/Users/ozgur/Documents/C++Projects/2DSnakeGame/cmake-build-debug/CMakeFiles/2DSnakeGame.dir/main.cpp.obj"
-  )
-set(CMAKE_CXX_COMPILER_ID "GNU")
-
-# Preprocessor definitions for this target.
-set(CMAKE_TARGET_DEFINITIONS_CXX
-  "SFML_STATIC"
-  )
-
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "C:/SFML/include"
   )
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/ozgur/Documents/C++Projects/2DSnakeGame/Bait.cpp" "CMakeFiles/2DSnakeGame.dir/Bait.cpp.obj" "gcc" "CMakeFiles/2DSnakeGame.dir/Bait.cpp.obj.d"
+  "C:/Users/ozgur/Documents/C++Projects/2DSnakeGame/Game.cpp" "CMakeFiles/2DSnakeGame.dir/Game.cpp.obj" "gcc" "CMakeFiles/2DSnakeGame.dir/Game.cpp.obj.d"
+  "C:/Users/ozgur/Documents/C++Projects/2DSnakeGame/Game_Block.cpp" "CMakeFiles/2DSnakeGame.dir/Game_Block.cpp.obj" "gcc" "CMakeFiles/2DSnakeGame.dir/Game_Block.cpp.obj.d"
+  "C:/Users/ozgur/Documents/C++Projects/2DSnakeGame/Snake.cpp" "CMakeFiles/2DSnakeGame.dir/Snake.cpp.obj" "gcc" "CMakeFiles/2DSnakeGame.dir/Snake.cpp.obj.d"
+  "C:/Users/ozgur/Documents/C++Projects/2DSnakeGame/Snake_Part.cpp" "CMakeFiles/2DSnakeGame.dir/Snake_Part.cpp.obj" "gcc" "CMakeFiles/2DSnakeGame.dir/Snake_Part.cpp.obj.d"
+  "C:/Users/ozgur/Documents/C++Projects/2DSnakeGame/main.cpp" "CMakeFiles/2DSnakeGame.dir/main.cpp.obj" "gcc" "CMakeFiles/2DSnakeGame.dir/main.cpp.obj.d"
   )
 
 # Targets to which this target links.
--- cmake-build-debug/CMakeFiles/2DSnakeGame.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/2DSnakeGame.dir/cmake_clean.cmake
@@ -3,11 +3,17 @@ file(REMOVE_RECURSE
   "2DSnakeGame.exe.manifest"
   "2DSnakeGame.pdb"
   "CMakeFiles/2DSnakeGame.dir/Bait.cpp.obj"
+  "CMakeFiles/2DSnakeGame.dir/Bait.cpp.obj.d"
   "CMakeFiles/2DSnakeGame.dir/Game.cpp.obj"
+  "CMakeFiles/2DSnakeGame.dir/Game.cpp.obj.d"
   "CMakeFiles/2DSnakeGame.dir/Game_Block.cpp.obj"
+  "CMakeFiles/2DSnakeGame.dir/Game_Block.cpp.obj.d"
   "CMakeFiles/2DSnakeGame.dir/Snake.cpp.obj"
+  "CMakeFiles/2DSnakeGame.dir/Snake.cpp.obj.d"
   "CMakeFiles/2DSnakeGame.dir/Snake_Part.cpp.obj"
+  "CMakeFiles/2DSnakeGame.dir/Snake_Part.cpp.obj.d"
   "CMakeFiles/2DSnakeGame.dir/main.cpp.obj"
+  "CMakeFiles/2DSnakeGame.dir/main.cpp.obj.d"
   "lib2DSnakeGame.dll.a"
 )
 
